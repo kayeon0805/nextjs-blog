71fbacc5e7e920dcdee609ca4e9059b5029f1303
kayeon0805 <kayeon8855@gmail.com>
2024-05-30 19:39:24 +0900

    docs: add first post

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <div className="prose dark:prose-invert">
       {allPosts.map((post) => (
-        <article key={post._id}>
+        <article key={post._id} className="border-t py-4 first:border-t-0 first:mt-10 md:py-6">
           <Link href={post.slug}>
-            <h2>{post.title}</h2>
+            <h2 className="mb-1 mt-0 text-lg md:text-2xl md:mb-2">{post.title}</h2>
           </Link>
-          {post.description && <p>{post.description}</p>}
+          {post.description && <p className="mt-0 text-sm md:text-base">{post.description}</p>}
         </article>
       ))}
     </div>
